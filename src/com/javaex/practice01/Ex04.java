679a307b437453e9ccc107eddb55949f2698aa50
y <y@DESKTOP-AD2QNB7>
2022-04-19 16:00:14 +0900

    1.0

---
+++ src/com/javaex/practice01/Ex04.java
@@ -0,0 +1,16 @@
+package com.javaex.practice01;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		int x,y;
+		
+		x = 10;
+		y = 20;
+		
+		int sum = x + y;
+		
+		System.out.println("합은 " + sum);
+	}
+
+}
